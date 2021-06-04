07aaeb2b04f6f30474880e8477a15a7a2ada413c
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2021-06-04 20:26:49 -0300

    feat : post on db when a user been registed

--- lib/app/modules/shared/auth/auth_store.dart
+++ lib/app/modules/shared/auth/auth_store.dart
@@ -1,15 +1,17 @@
-import 'package:finished_games_register/app/modules/shared/auth/repositories/auth_repository_interface.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:finished_games_register/app/modules/shared/auth/repositories/auth_repository.dart';
+import 'package:finished_games_register/app/modules/shared/services/users/users_api_interface.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+
 
 part 'auth_store.g.dart';
 
 class AuthStore = _AuthStoreBase with _$AuthStore;
 
 abstract class _AuthStoreBase with Store {
-  final IAuthRepository _authRepository = Modular.get();
+  final AuthRepository _authRepository = Modular.get();
+  final IUsersApi _usersApi = Modular.get();
 
   @observable
   UserCredential user;
@@ -26,6 +28,7 @@ abstract class _AuthStoreBase with Store {
   Future singupByEmailPassword(email, password) async {
     user = (await _authRepository.singupByEmailPassword(email, password));
     setUser(user);
+    _usersApi.postUser(user);
   }
 
   Future singinByEmailPassword(email, password) async {
