b3aef316030d0ed3382fef5414d56c8572c1d945
NityanandaBarbosa <nityanandabarbosa@gmail.com>
2021-06-25 21:02:23 -0300

    fix :  add or edit when a publisher has not a closed date

--- lib/app/modules/list/game/game_store.dart
+++ lib/app/modules/list/game/game_store.dart
@@ -10,8 +10,8 @@ import 'entities/game_model.dart';
 part 'game_store.g.dart';
 
 class GameStore = _GameStoreBase with _$GameStore;
-abstract class _GameStoreBase with Store {
 
+abstract class _GameStoreBase with Store {
   final AuthStore _auth = Modular.get();
   final IGameApi _gameApi = Modular.get();
 
@@ -45,8 +45,7 @@ abstract class _GameStoreBase with Store {
   @action
   setPubChoice(var value) => pubChoice = value;
 
-
-  void setGameValues(){
+  void setGameValues() {
     DateTime dtRelease = DateTime.parse(game.releaseDate);
     releaseDate = dtRelease;
     idPub = game.idPub;
@@ -61,22 +60,37 @@ abstract class _GameStoreBase with Store {
     } else if (returnResponse == null) {
       ShowAlertDialog(context, 'Could not connect to server!');
       return null;
-    } else if(returnResponse != "none") {
+    } else if (returnResponse != "none") {
       return true;
     }
   }
 
   Future verifyFields(context) async {
-    if (gameName == null || gameName == "" || releaseDate == null || pubChoice == null) {
+    bool closedControl = true;
+
+    if (gameName == null ||
+        gameName == "" ||
+        releaseDate == null ||
+        pubChoice == null) {
       return false;
     } else {
-    if((releaseDate.compareTo(DateTime.parse(pubChoice.foundingDate))>0) && (releaseDate.compareTo(DateTime.parse(pubChoice.closedDate))<0)){
-        if(game == null){
-          response = await _gameApi.postGame(_auth.myId, pubChoice.idPub,gameName, releaseDate.toString());
-        }else{
-          response = await _gameApi.putGame(_auth.myId, pubChoice.idPub,game.idGame,gameName, releaseDate.toString());
+      try{
+        releaseDate.compareTo(DateTime.parse(pubChoice.closedDate)) < 0
+            ? closedControl = true
+            : closedControl = false;
+      }catch(e){
+        closedControl = true;
+      }
+      if ((releaseDate.compareTo(DateTime.parse(pubChoice.foundingDate)) > 0) &&
+          (closedControl == true)) {
+        if (game == null) {
+          response = await _gameApi.postGame(
+              _auth.myId, pubChoice.idPub, gameName, releaseDate.toString());
+        } else {
+          response = await _gameApi.putGame(_auth.myId, pubChoice.idPub,
+              game.idGame, gameName, releaseDate.toString());
         }
-      }else{
+      } else {
         ShowAlertDialog(context, "Release Date is invalid!");
         return "none";
       }
@@ -84,13 +98,12 @@ abstract class _GameStoreBase with Store {
     }
   }
 
-  Future delete() async{
-    try{
+  Future delete() async {
+    try {
       var response = await _gameApi.deleteGame(_auth.myId, game.idGame);
       return response;
-    }catch(e){
+    } catch (e) {
       return null;
     }
-
   }
 }
